c216bb927b39963bd87c550b22737b544233a28e
ocegik <147722344+ocegik@users.noreply.github.com>
2026-01-09 22:01:20 +0530

    Add Note model and update HelloController to return a Note object

--- notes-api/src/main/java/com/tarun/notes/controller/HelloController.java
+++ notes-api/src/main/java/com/tarun/notes/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.tarun.notes.controller;
 
+import com.tarun.notes.model.Note;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from backend";
+    @GetMapping("/note")
+    public Note getNote() {
+        return new Note(
+                1,
+                "First Note",
+                "This note is coming from backend"
+        );
     }
 }
